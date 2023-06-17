549a2942b8a60e7bcff311dd8687a516ab9fa8a6
Mohit <mohitsaini3012003@gmail.com>
2023-06-17 15:20:54 +0530

    rotating an array by one index

---
+++ rotateMatrix.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+void rotateMatrix(vector<vector<int>> &mat, int n, int m)
+{
+    int top=0,bottom=n-1,left=0,right=m-1;
+    while(left<right&&top<bottom)
+    {
+        int temp=mat[top][left];
+        for(int i=left+1;i<=right;i++)
+        {
+            int k=mat[top][i];
+            mat[top][i]=temp;
+            temp=k;
+        }
+        top++;
+        for(int i=top;i<=bottom;i++)
+        {
+            int k=mat[i][right];
+            mat[i][right]=temp;
+            temp=k;
+        }
+        right--;
+        for(int i=right;i>=left;i--)
+        {
+            int k=mat[bottom][i];
+            mat[bottom][i]=temp;
+            temp=k;
+        }
+        bottom--;
+        for(int i=bottom;i>=top;i--)
+        {
+            int k=mat[i][left];
+            mat[i][left]=temp;
+            temp=k;
+        }
+        left++;
+        mat[top-1][left-1]=temp;
+    }
+}
